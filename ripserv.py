3e1f708d819c632f10b1153ffc0abf8f4bbd711c
pallenpost@gmail.com <pallenpost@gmail.com@39299a86-776b-00a5-7e9d-91d3eb6c566e>
2012-09-24 00:57:30 +0000

    Added license

--- ripserv.py
+++ ripserv.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+#
+# Python-RIPv2 -- A Python implementation of RIPv2.
+# Copyright (C) 2012 Patrick F. Allen
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 import struct
 import ipaddr
@@ -6,11 +23,11 @@ import sys
 import subprocess
 import re
 import optparse
-from twisted.internet import protocol
-from twisted.internet import reactor
+import binascii
 import logging
 import logging.config
-import binascii
+from twisted.internet import protocol
+from twisted.internet import reactor
 
 class RIP(protocol.DatagramProtocol):
     """An implementation of RIPv2 using the twisted asynchronous networking
