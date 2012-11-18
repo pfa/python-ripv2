5c421f5a0d5d35a620ce8167cc1deb823858bdee
pallenpost@gmail.com <pallenpost@gmail.com@39299a86-776b-00a5-7e9d-91d3eb6c566e>
2012-11-18 04:19:17 +0000

    Updated rip_request.py to support requesting both the whole table and specific routes.

--- test/rip_request.py
+++ test/rip_request.py
@@ -13,26 +13,57 @@ import ripserv
 
 def main(argv):
     options, arguments = parse_args(argv)
+
+    # Create request
     hdr = ripserv.RIPHeader(cmd=ripserv.RIPHeader.TYPE_REQUEST, ver=2)
-    rtes = []
-    for requested_rt in options.route:
-        ip = ipaddr.IPv4Network(requested_rt)
-        rtes.append(ripserv.RIPRouteEntry(address=ip.ip.exploded,
-                    mask=ip.prefixlen, nexthop="0.0.0.0", metric=0, tag=0))
+    if options.specific_routes:
+        rtes = create_specific_rtes(options.route)
+    elif options.whole_table:
+        rtes = create_whole_rtes()
     request = ripserv.RIPPacket(hdr=hdr, rtes=rtes)
 
+    # Send request
     sock = socket.socket(type=socket.SOCK_DGRAM)
     sock.connect((options.dst, 520))
     sock.send(request.serialize())
     sock.settimeout(5)
 
+    # Read response
     # XXX Doesn't currently deal with >25 routes.
-    buf = sock.recv(1024)
+    try:
+        buf = sock.recv(1024)
+    except socket.timeout:
+        print("Did not receive a response from remote router.")
+        return -1
+    except socket.error:
+        print("Error sending to the remote router. (Is a RIP "
+              "service listening at the destination?)")
+        return -1
+
     response = ripserv.RIPPacket(data=buf, src_ip="0.0.0.0")
 
-    validate_response(request, response)
+    if not options.quiet:
+        print("Response contained:")
+        for rte in response.rtes:
+            print(rte)
 
-def validate_response(request, response):
+    # Validate response
+    return validate_response(request, response, options)
+
+def create_specific_rtes(routes):
+    rtes = []
+    for requested_rt in routes:
+        ip = ipaddr.IPv4Network(requested_rt)
+        rtes.append(ripserv.RIPRouteEntry(address=ip.ip.exploded,
+                    mask=ip.prefixlen, nexthop="0.0.0.0", metric=0, tag=0))
+    return rtes
+
+def create_whole_rtes():
+    return [ripserv.RIPRouteEntry(afi=0, address="0.0.0.0", mask=0, tag=0,
+                                  metric=ripserv.RIPRouteEntry.MAX_METRIC,
+                                  nexthop="0.0.0.0")]
+
+def validate_response(request, response, options):
     pass_validation = True
 
     if response.hdr.cmd != ripserv.RIPHeader.TYPE_RESPONSE:
@@ -41,13 +72,41 @@ def validate_response(request, response):
     if response.hdr.ver != 2:
         print("Response had an unexpected ver field: %d" % response.hdr.ver)
         pass_validation = False
-    if len(response.rtes) != len(request.rtes):
-        print("Different number of request vs response RTEs, %d vs %d." % \
-              (len(response.rtes), len(request.rtes)))
-        pass_validation = False
 
+    if options.specific_routes:
+        if not validate_specific_routes(request, response):
+            pass_validation = False
+    elif options.whole_table:
+        if not validate_whole_table(response):
+            pass_validation = False
+
+    if pass_validation:
+        print("Response passed validation.")
+    else:
+        print("Warning: Response FAILED to pass validation.")
+
+    return pass_validation
+
+def validate_whole_table(response):
+    # Just check for sane ranges... whether or not the routes are expected
+    # depends on the rest of the network and the router's configuration.
+    pass_validation = True
+
+    for rte in response.rtes:
+        if rte.metric > ripserv.RIPRouteEntry.MAX_METRIC:
+            print("Reported metric is above the maximum.")
+            pass_validation = False
+        if rte.tag:
+            print("Tag not set to 0.")
+            pass_validation = False
+
+    return pass_validation
+
+def validate_specific_routes(request, response):
     # Other than the metrics and nexthops (nexthops are changed locally),
     # everything should be identical in the rte lists.
+    pass_validation = True
+
     for request_rte in request.rtes:
         rte_found = False
         for response_rte in response.rtes:
@@ -57,14 +116,14 @@ def validate_response(request, response):
                 rte_found = True
                 break
         if not rte_found:
-            if rte not in response.rtes:
+            if response_rte not in response.rtes:
                 print("Missing RTE in response: %s" % rte)
                 pass_validation = False
 
-    if pass_validation:
-        print("Response passed validation.")
-    else:
-        print("Warning: Response FAILED to pass validation.")
+    if len(response.rtes) != len(request.rtes):
+        print("Different number of request vs response RTEs, %d vs %d." % \
+              (len(response.rtes), len(request.rtes)))
+        pass_validation = False
 
     return pass_validation
 
@@ -74,13 +133,32 @@ def parse_args(argv):
                   action="append")
     op.add_option("-d", "--dst", help="The router to request from.",
                   type="string")
+    op.add_option("-w", "--whole-table", default=False,
+                  action="store_true",
+                  help="Request the whole routing table.")
+    op.add_option("-s", "--specific-routes", default=False,
+                  action="store_true",
+                  help="Request specific routes (use -r).")
+    op.add_option("-q", "--quiet", default=False,
+                  action="store_true",
+                  help="Only print validation messages.")
     options, arguments = op.parse_args()
+
     if arguments:
         op.error("No non-option arguments are expected.")
-    if not options.route:
-        op.error("At least one route to request is required (-r).")
-    if len(options.route) > 25:
-        op.error("Only <25 routes supported currently.")
+
+    if options.specific_routes and options.whole_table:
+        op.error("Options -w and -s are mutually exclusive.")
+    elif not options.specific_routes and not options.whole_table:
+        op.error("Exactly one of -w or -s is required.")
+    if options.specific_routes:
+        if not options.route:
+            op.error("At least one route to request is required (-r).")
+        if len(options.route) > 25:
+            op.error("Only <25 routes supported currently.")
+    if options.whole_table:
+        if options.route:
+            op.error("No -r arguments are needed if -w is specified.")
     if not options.dst:
         op.error("The destination router must be specified (-d).")
 
